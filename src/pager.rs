bcc2381978bf35a6911928d302848193f5fc14da
mircea <mircea@pop-os.home>
2026-03-20 22:13:04 +0100

    format pager and fix clippy for it

--- src/pager.rs
+++ src/pager.rs
@@ -1,4 +1,4 @@
-use std::{collections::BTreeMap, io::{Write}};
+use std::{collections::BTreeMap, io::Write};
 
 use chrono::Datelike;
 use crossterm::{
@@ -18,18 +18,19 @@ pub enum ChunkError {
     UnexpectedFileEnd,
 }
 
-pub fn get_next_chunk(lines: &mut std::iter::Peekable<std::str::Lines<'_>>) -> Result<QuestionChunk, ChunkError>{
+pub fn get_next_chunk(
+    lines: &mut std::iter::Peekable<std::str::Lines<'_>>,
+) -> Result<QuestionChunk, ChunkError> {
     let mut this_chunk_str: String = "".to_string();
 
     while let Some(line) = lines.next() {
         if !line.is_empty() {
             this_chunk_str += line;
             this_chunk_str += "\n";
-        } 
-        else if this_chunk_str.trim().is_empty() {
+        } else if this_chunk_str.trim().is_empty() {
             return Err(ChunkError::EmptyChunk);
         }
-        if lines.peek().is_none() || line.is_empty()  {
+        if lines.peek().is_none() || line.is_empty() {
             this_chunk_str = this_chunk_str.trim().to_string();
             let this_chunk = QuestionChunk::from(this_chunk_str.clone());
 
@@ -43,71 +44,71 @@ pub fn get_next_chunk(lines: &mut std::iter::Peekable<std::str::Lines<'_>>) -> R
     Err(ChunkError::UnexpectedFileEnd)
 }
 
-pub fn parse_date_file(content: &String) -> std::io::Result<String> {
+pub fn parse_date_file(content: &str) -> std::io::Result<String> {
     let mut invalid_chunks_number = 0;
     let mut notes: Vec<String> = Vec::new();
     let mut descriptions: Vec<String> = Vec::new();
     let mut ratings: Vec<String> = Vec::new();
-    let mut long_questions: BTreeMap<String, Vec<String>>= BTreeMap::new();
-    let mut short_questions: BTreeMap<String, Vec<String>>= BTreeMap::new();
+    let mut long_questions: BTreeMap<String, Vec<String>> = BTreeMap::new();
+    let mut short_questions: BTreeMap<String, Vec<String>> = BTreeMap::new();
 
     let mut lines: std::iter::Peekable<std::str::Lines<'_>> = content.lines().peekable();
 
     loop {
         match get_next_chunk(&mut lines) {
             Ok(this_chunk) => {
-                let chunk_type = this_chunk.get_type().unwrap_or_else(|_| { QuestionType::Empty});
+                let chunk_type = this_chunk
+                    .get_type()
+                    .unwrap_or(QuestionType::Empty);
 
                 let mut answer_iter = this_chunk.get_answer()?.into_iter();
                 let info = this_chunk.get_informative()?.get_text()?;
                 let prompt_type = this_chunk.get_prompt_type()?;
 
                 if prompt_type == PromptQuestionType::Rating {
-                    while let Some(question) = answer_iter.next() {
+                    for question in answer_iter.by_ref() {
                         ratings.push(format!("[{}] {}", info, question.get_text()?));
                     }
-                } 
-                else if prompt_type == PromptQuestionType::Description {
+                } else if prompt_type == PromptQuestionType::Description {
                     if let Some(question) = answer_iter.next() {
                         descriptions.push(format!("    [{}] {}", info, question.get_text()?));
                     }
 
-                    while let Some(question) = answer_iter.next() {
+                    for question in answer_iter.by_ref() {
                         descriptions.push(format!("  {}", question.get_text()?));
                     }
-                } 
-                else if prompt_type == PromptQuestionType::Note {
+                } else if prompt_type == PromptQuestionType::Note {
                     if let Some(question) = answer_iter.next() {
                         notes.push(format!("    [{}] {}", info, question.get_text()?));
                     }
-                    while let Some(question) = answer_iter.next() {
+
+                    for question in answer_iter.by_ref() {
                         notes.push(format!("  {}", question.get_text()?));
                     }
-                } 
-                else if chunk_type == QuestionType::Long {
+                } else if chunk_type == QuestionType::Long {
                     let question_text = this_chunk.get_question()?.get_text()?;
                     let mut answer: String = "".to_string();
 
                     if let Some(question) = answer_iter.next() {
-                        let answer_line = format!("    [{}] {}", info, question.get_text()?); 
+                        let answer_line = format!("    [{}] {}", info, question.get_text()?);
                         answer += answer_line.as_str();
                     }
 
-                    while let Some(question) = answer_iter.next() {
-                        let answer_line = format!("\n  {}", question.get_text()?); 
+                    for question in answer_iter.by_ref() {
+                        let answer_line = format!("\n  {}", question.get_text()?);
                         answer += answer_line.as_str();
                     }
 
-                    long_questions.entry(question_text)
-                        .or_insert(Vec::new())
+                    long_questions
+                        .entry(question_text)
+                        .or_default()
                         .push(answer);
-
-                }
-                else {
-                    while let Some(question) = answer_iter.next() {
+                } else {
+                    for question in answer_iter.by_ref() {
                         let question_text = this_chunk.get_question()?.get_text()?;
-                        short_questions.entry(question_text)
-                            .or_insert(Vec::new())
+                        short_questions
+                            .entry(question_text)
+                            .or_default()
                             .push(format!("[{}] {}", info, question.get_text()?));
                     }
                 }
@@ -163,14 +164,17 @@ pub fn parse_date_file(content: &String) -> std::io::Result<String> {
     }
 
     if invalid_chunks_number != 0 {
-        return_content +=
-            format!("Encountered {} invalid chunk(s) in file!", invalid_chunks_number).as_str();
+        return_content += format!(
+            "Encountered {} invalid chunk(s) in file!",
+            invalid_chunks_number
+        )
+        .as_str();
     }
 
     Ok(return_content)
 }
 
-pub fn format_content(content: &String) -> std::io::Result<Vec<String>> {
+pub fn format_content(content: &str) -> std::io::Result<Vec<String>> {
     let (term_width, _) = terminal::size()?;
 
     let mut terminal_lines: Vec<String> = Vec::new();
@@ -209,9 +213,9 @@ pub fn format_content(content: &String) -> std::io::Result<Vec<String>> {
 }
 
 pub fn write_display_content(
-    path: &std::path::PathBuf,
+    path: &std::path::Path,
     height_index: usize,
-    terminal_lines: &Vec<String>,
+    terminal_lines: &[String],
     mut stdout: &std::io::Stdout,
 ) -> std::io::Result<()> {
     execute!(stdout, terminal::Clear(terminal::ClearType::All))?;
@@ -220,34 +224,34 @@ pub fn write_display_content(
     let mut line_y = 0;
     let mut path_str: String = "".to_string();
 
-    if let Some(stem_os) = path.file_stem() {
-        if let Some(stem_str) = stem_os.to_str() {
-            let weekday = chrono::NaiveDate::parse_from_str(stem_str, "%Y-%m-%d").unwrap().weekday();
-            path_str = format!("{} {}", stem_str, weekday.to_string().as_str());
-        }
+    if let Some(stem_os) = path.file_stem() 
+        && let Some(stem_str) = stem_os.to_str() 
+    {
+        let weekday = chrono::NaiveDate::parse_from_str(stem_str, "%Y-%m-%d")
+            .unwrap()
+            .weekday();
+        path_str = format!("{} {}", stem_str, weekday.to_string().as_str());
     }
     queue!(
         stdout,
-        cursor::MoveTo(0 as u16, line_y),
+        cursor::MoveTo(0, line_y),
         style::PrintStyledContent(path_str.white())
     )?;
 
     let init_line_y = 2;
     line_y = init_line_y;
 
-    let mut line_idx = 0;
-    for line in terminal_lines {
+    for (line_idx, line ) in terminal_lines.iter().enumerate() {
         if line_idx >= height_index
             && line_idx < height_index + term_height as usize - init_line_y as usize
         {
             queue!(
                 stdout,
-                cursor::MoveTo(0 as u16, line_y),
+                cursor::MoveTo(0, line_y),
                 style::PrintStyledContent(line.clone().white())
             )?;
             line_y += 1;
         }
-        line_idx += 1;
     }
 
     std::io::stdout().flush()?;
